d60d4fc1b526149bb5f9d383b948828e06144815
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 09:18:14 -0400

    Final program

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,14 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //num birthMonth = 0
+
         Scanner in = new Scanner(System.in);
         int birthMonth = 0;
         String trash = "";
 
-        //output “Enter your birth month: ”
 
         System.out.println("Enter your birth month: ");
 
-        //input birthMonth
         if (in.hasNextInt())
         {
             birthMonth = in.nextInt();
@@ -37,11 +35,5 @@ public class Main
             System.out.println("\nPlease run the program again.");
             System.exit(0);
         }
-
-        //if birthMonth >= 1 AND birthMonth <= 12 then
-        //output “Your birth month is: ”+ birthMonth
-        //else
-        //output “The month value entered is incorrect: ” + birthMonth
-        //endIf
     }
 }
