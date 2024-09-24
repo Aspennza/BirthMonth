1606c5a45ed2b5521a0a10307ae517c47069c1b6
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 09:00:34 -0400

    Scanner and variables entered

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        //num birthMonth = 0
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash = "";
+
+        //output “Enter your birth month: ”
+
+        System.out.println("Enter your birth month: ");
+
+        if (in.hasNextInt())
+        {
+
+        }
+
+
+        //input birthMonth
+        //if birthMonth >= 1 AND birthMonth <= 12 then
+        //output “Your birth month is: ”+ birthMonth
+        //else
+        //output “The month value entered is incorrect: ” + birthMonth
+        //endIf
     }
 }
