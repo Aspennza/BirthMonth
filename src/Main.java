cfd41163374f7820287fa66b978795ff0f63dfd2
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 09:09:16 -0400

    Program draft complete--comments still present

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,31 @@ public class Main
 
         System.out.println("Enter your birth month: ");
 
+        //input birthMonth
         if (in.hasNextInt())
         {
-
+            birthMonth = in.nextInt();
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nPlease enter an integer value. You entered: " + trash + ".");
+            System.out.println("\nPlease run the program again.");
+            System.exit(0);
         }
 
+        if (birthMonth >= 1 && birthMonth <= 12)
+        {
+            System.out.println("\nYour birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("\nThe month value entered is incorrect: " + birthMonth);
+            System.out.println("\nPlease run the program again.");
+            System.exit(0);
+        }
 
-        //input birthMonth
         //if birthMonth >= 1 AND birthMonth <= 12 then
         //output “Your birth month is: ”+ birthMonth
         //else
